396775d8f42fc2a2b41db0ae4df04d380e5f1ca0
정석영 <aswe0409@naver.com>
2024-08-09 13:22:55 +0900

    feat: spring boot fastapi connection

---
+++ backend/src/main/java/com/ssafy/buddy/member/controller/response/IdcardCheckResponse.java
@@ -0,0 +1,14 @@
+package com.ssafy.buddy.member.controller.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class IdcardCheckResponse {
+    private String allow;
+}
