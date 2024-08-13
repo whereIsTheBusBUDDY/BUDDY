80e7d5ab112d97d2f6d5ad71ecd5cc62a3e2b240
정지원 <z1gram@naver.com>
2024-08-13 13:56:19 +0900

    introduce screen css

--- frontend/src/screen/IntroduceScreen.js
+++ frontend/src/screen/IntroduceScreen.js
@@ -15,57 +15,62 @@ import { useNavigation } from '@react-navigation/native';
 const IntroduceScreen = () => {
   const navigation = useNavigation();
   return (
-    <ScrollView contentContainerStyle={styles.container}>
-      <View style={styles.imageContainer}>
-        <Image
-          source={require('../../assets/001.png')}
-          style={styles.firstImage}
-          resizeMode="cover"
+    <View style={styles.container}>
+      <ScrollView contentContainerStyle={styles.scrollViewContent}>
+        <View style={styles.imageContainer}>
+          <Image
+            source={require('../../assets/001.png')}
+            style={styles.firstImage}
+            resizeMode="cover"
+          />
+          <Image
+            source={require('../../assets/002.png')}
+            style={styles.image}
+            resizeMode="cover"
+          />
+          <Image
+            source={require('../../assets/003.png')}
+            style={styles.image}
+            resizeMode="cover"
+          />
+          <Image
+            source={require('../../assets/004.png')}
+            style={styles.image}
+            resizeMode="cover"
+          />
+        </View>
+      </ScrollView>
+      <View style={styles.fixedContainer}>
+        <Button
+          title="회원가입하러 가기"
+          onPress={() => navigation.navigate('IDScreen')}
+          buttonColor={ButtonColors.GRAY}
+          buttonStyle={styles.navigationButton}
         />
-        <Image
-          source={require('../../assets/002.png')}
-          style={styles.image}
-          resizeMode="cover"
-        />
-        <Image
-          source={require('../../assets/003.png')}
-          style={styles.image}
-          resizeMode="cover"
-        />
-        <Image
-          source={require('../../assets/004.png')}
-          style={styles.image}
-          resizeMode="cover"
-        />
+        <TouchableOpacity onPress={() => navigation.navigate('Login')}>
+          <Text style={styles.linkText}>
+            이미 회원이신가요?{' '}
+            <Text style={styles.highlightText}>로그인 바로가기</Text>
+          </Text>
+        </TouchableOpacity>
       </View>
-      <Button
-        title="회원가입하러 가기"
-        onPress={() => navigation.navigate('IDScreen')}
-        buttonColor={ButtonColors.GRAY}
-        buttonStyle={styles.navigationButton}
-      />
-      <TouchableOpacity onPress={() => navigation.navigate('Login')}>
-        <Text style={styles.linkText}>
-          이미 회원이신가요?{' '}
-          <Text style={styles.highlightText}>로그인 바로가기</Text>
-        </Text>
-      </TouchableOpacity>
-    </ScrollView>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flexGrow: 1,
-    alignItems: 'center',
-    paddingVertical: 0,
+    flex: 1,
     backgroundColor: WHITE,
+  },
+  scrollViewContent: {
     paddingVertical: 30,
+    paddingBottom: 120,
   },
   firstImage: {
     width: Dimensions.get('window').width,
     height: Dimensions.get('window').height / 1.5,
-    marginBottom: 70,
+    marginTop: -30,
   },
   image: {
     width: Dimensions.get('window').width,
@@ -80,6 +85,14 @@ const styles = StyleSheet.create({
   },
   highlightText: {
     color: PRIMARY.DEFAULT,
+  },
+  fixedContainer: {
+    position: 'absolute',
+    bottom: 10,
+    left: 0,
+    right: 0,
+    padding: 10,
+    alignItems: 'center',
   },
 });
 
