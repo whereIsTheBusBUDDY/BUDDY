9d3e8fab0d24a06a7044f4b4851086007aac957e
이승민 <tmdals77117@naver.com>
2024-08-14 09:38:42 +0900

    Fe issue 0126

--- frontend/src/api/url.js
+++ frontend/src/api/url.js
@@ -4,3 +4,6 @@ export const mm_url =
 export const sseUrl = 'http://i11b109.p.ssafy.io:8080/subscribe';
 
 export const routeUrl = 'http://54.180.242.7:8080';
+
+// 기본 주소
+export const BASEurl = 'https://ssafy.site:443';
